a80888467f799a8ade3a846317baaea146265d04
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-20 03:42:16 -0300

    Update Index.cshtml.cs

--- SiteContagem/Pages/Index.cshtml.cs
+++ SiteContagem/Pages/Index.cshtml.cs
@@ -22,8 +22,8 @@ public class IndexModel : PageModel
 
         // FIXME: trecho criado para simular falhas e permitir
         // a análise de problemas com a ferramenta k6s
-        if (valorAtual % 4 == 0)
-            throw new Exception("Simulação de falha");
+        //if (valorAtual % 4 == 0)
+        //    throw new Exception("Simulação de falha");
 
         TempData["Contador"] = valorAtual;
         TempData["Local"] = _CONTADOR.Local;
